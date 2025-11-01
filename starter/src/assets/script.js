af771a3cf59739252e04172ac8d6cd6786e2e9ee
TalaAlhendi <talaalhendiuni4@gmail.com>
2025-11-02 00:44:15 +0200

    done the shopping cart with standout functions

--- starter/src/assets/script.js
+++ starter/src/assets/script.js
@@ -1,18 +1,19 @@
 /* Create an array named products which you will use to add all of your product object literals that you create in the next step. */
+
 const products = [
   {
     name: "Cherry",
     price: 2.99,
     quantity: 0,
     productId: 1,
-    image: "/assets/images/cherry.jpg"
+    image: "images/cherry.jpg"
   },
   {
     name: "Orange",
     price: 1.99,
     quantity: 0,
     productId: 2,
-    image: "assets/images/orange.jpg"
+    image: "images/orange.jpg"
 
   },
   {
@@ -20,25 +21,29 @@ const products = [
     price: 3.49,
     quantity: 0,
     productId: 3,
-    image: "/assets/images/strawberry.jpg"
+    image: "images/strawberry.jpg"
   }
 
 ];
-
+// an empty array named cart holds the products added to the shopping cart.
 let cart = [];
 
+// add product to cart function finds product by id and adds it to cart and increases quantity to 1
 function addProductToCart(addProductId)
 {
   const product = products.find(item => item.productId === addProductId);
-  if (product) {
+  if (product) 
+  {
     product.quantity++;
     const cartItem = cart.find(item => item.productId === addProductId);
-    if (!cartItem) {
+    if (!cartItem) 
+    {
       cart.push(product);
     }
   }
 }
 
+// increase quantity function increases quantity of product in cart by 1 each time
 function increaseQuantity(productId) 
 {
   const product = products.find(item => item.productId === productId);
@@ -48,7 +53,7 @@ function increaseQuantity(productId)
   }
 }
 
-
+// decrease quantity function decreases quantity of product in cart by 1 each time 
 function decreaseQuantity(productId)
 {
   const product = products.find(item => item.productId === productId);
@@ -66,7 +71,7 @@ function decreaseQuantity(productId)
   }
   
 }
-
+// remove product from cart function removes product from cart regardless of quantity
 function removeProductFromCart(productId)
 {
   const product = products.find(item => item.productId === productId);
@@ -82,6 +87,7 @@ function removeProductFromCart(productId)
   }
 }
 
+// cart total function calculates total cost of all products in cart
 function cartTotal()
 {
   let total =0;
@@ -92,13 +98,14 @@ function cartTotal()
 
   return total;
 }
-/* Create a function called emptyCart that empties the products from the cart */
+// function emptyCart empties the products from the cart
 function emptyCart()
 {
   cart.forEach(item => item.quantity = 0);
   cart = [];
 }
 
+// pay function takes an amount and returns the change after paying for the cart total
 function pay(amount)
 {
   const totalCost = cartTotal();
@@ -113,6 +120,26 @@ function pay(amount)
 
 /* Place stand out suggestions here (stand out suggestions can be found at the bottom of the project rubric.)*/
 
+// currency converter function converts product prices based on selected currency
+function currency(selectedCurrency) {
+    switch(selectedCurrency) {
+        case 'EUR':
+            products.forEach(p => p.price = +(p.price * 0.87).toFixed(2)); 
+            break;
+        case 'YEN':
+            products.forEach(p => p.price = +(p.price * 154.01).toFixed(2)); 
+            break;
+        default: // USD
+            products.forEach(p => {
+                
+                if(p.productId === 1) p.price = 2.99;
+                if(p.productId === 2) p.price = 1.99;
+                if(p.productId === 3) p.price = 3.49;
+            });
+            break;
+    }
+}
+
 
 /* The following is for running unit tests. 
    To fully complete this project, it is expected that all tests pass.
@@ -120,6 +147,7 @@ function pay(amount)
    npm run test
 */
 
+// module.exports allows the functions and variables to be used in other files
 module.exports = {
    products,
    cart,
@@ -131,5 +159,5 @@ module.exports = {
    pay, 
    emptyCart,
    /* Uncomment the following line if completing the currency converter bonus */
-   //currency
+   currency
 }
